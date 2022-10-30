f1f888c9e2440cff9810738b20eb469d3682325a
Nie-Mand <chill.out.x19@gmail.com>
2022-10-30 07:59:04 +0100

    ok

--- src/core/hero/index.tsx
+++ src/core/hero/index.tsx
@@ -103,13 +103,13 @@ export function HeroImageBackground() {
 
   return (
     <div className={classes.wrapper}>
-      <Overlay color="#000" opacity={0.65} zIndex={-1} />
+      <Overlay color="#000" opacity={0.8} zIndex={-1} />
 
       <div className={classes.inner}>
         <Title className={classes.title}>
-          Realtime Crisis{' '}
+          The Help Pool{' '}
           <Text component="span" inherit className={classes.highlight}>
-            Detection
+            At time of CRISES
           </Text>
         </Title>
 
